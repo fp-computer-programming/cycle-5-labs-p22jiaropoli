062fa98a33152d9cb865c8c350556005845e9cc2
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-26 10:15:53 -0400

    Add file

---
+++ lab-5-2.py
@@ -0,0 +1,15 @@
+# Author JRI 10/26/21
+
+import math
+
+x1 = math.ceil(7.15)
+x2 = math.floor(19.156)
+x3 = math.fabs(-42)
+x4 = math.pow(2, 3)
+x5 = math.log10(100)
+
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
