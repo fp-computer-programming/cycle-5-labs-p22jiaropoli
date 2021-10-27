ff055c34bcb139fd67c24065fc2ede9df0b4475d
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-27 10:20:00 -0400

    Add file

--- lab-5-4.py
+++ lab-5-4.py
@@ -4,9 +4,9 @@ print('F' + 'a' + 'i' + 'r' + 'f' + 'i' + 'e' + 'l' + 'd' + ' ' + 'P' + 'r' + 'e
 
 print("-" * 20)
 
-print('''I'm loving this short story I'm reading, "The Fall of the House of Usher"''')
+print("I'm loving this short story I'm reading, \"The Fall of the House of Usher\"")
 
-print(len('''I'm loving this short story I'm reading, "The Fall of the House of Usher"'''))
+print(len("I'm loving this short story I'm reading, \"The Fall of the House of Usher\""))
 
 x = "apple"
 print(x[3])
