1002736e4c275fe9c363bc5c3c9b7916ee4e9a3d
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-29 10:46:20 -0400

    Add file

---
+++ lab-5-6-1.py
@@ -0,0 +1,8 @@
+# Author JRI 10/29/21
+
+x = "flibbertigibbet"
+t = x.find("t")
+print(t)
+
+fish = "I wish, I wish, I was a fish."
+print(fish.split(","))
---
+++ lab5-6-2.py
@@ -0,0 +1,6 @@
+# Author JRI 10/29/21
+
+user1 = input("User 1, what is your first name? ")
+user2 = input("User 2, what is your first name? ")
+
+print("Hello, {0}. My name is {1}.".format(user1, user2))
